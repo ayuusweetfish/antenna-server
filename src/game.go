8a467d6ddf7fa61632afc0fabb33708246d8bd95
Ayu <claire@ayu.land>
2024-04-03 01:31:39 +0800

    Implement seat/withdraw

--- src/game.go
+++ src/game.go
@@ -45,8 +45,7 @@ type GameplayState struct {
 	PhaseStatus interface{}
 }
 
-func (s GameplayState) Repr(userId int) OrderedKeysMarshal {
-	// Players
+func (s GameplayState) PlayerReprs() []OrderedKeysMarshal {
 	playerReprs := []OrderedKeysMarshal{}
 	for _, p := range s.Players {
 		if p.Profile.Id > 0 {
@@ -58,6 +57,11 @@ func (s GameplayState) Repr(userId int) OrderedKeysMarshal {
 			})
 		}
 	}
+	return playerReprs
+}
+func (s GameplayState) Repr(userId int) OrderedKeysMarshal {
+	// Players
+	playerReprs := s.PlayerReprs()
 
 	// Phase
 	var phaseName string
@@ -86,6 +90,27 @@ func (s GameplayState) Repr(userId int) OrderedKeysMarshal {
 		entries = append(entries, OrderedKeysEntry{phaseName + "_status", statusRepr})
 	}
 	return entries
+}
+
+func (s *GameplayState) Seat(user User, profile Profile) string {
+	// Check duplicate
+	for _, p := range s.Players {
+		if p.User.Id == user.Id {
+			return "Already seated"
+		}
+	}
+	s.Players = append(s.Players, GameplayPlayer{User: user, Profile: profile})
+	return ""
+}
+
+func (s *GameplayState) WithdrawSeat(userId int) string {
+	for i, p := range s.Players {
+		if p.User.Id == userId {
+			s.Players = append(s.Players[:i], s.Players[i+1:]...)
+			return ""
+		}
+	}
+	return "Not seated"
 }
 
 ////// Room //////
@@ -134,11 +159,25 @@ func (r *GameRoom) Lost(userId int) {
 func (r *GameRoom) StateMessage(userId int) OrderedKeysMarshal {
 	user := r.Conns[userId].User
 	entries := OrderedKeysMarshal{
+		{"type", "room_state"},
 		{"user", user.Repr()}, // Debug usage
 		{"room", r.Room.Repr()},
 	}
 	entries = append(entries, r.Gameplay.Repr(userId)...)
 	return entries
+}
+
+func (r *GameRoom) BroadcastAssemblyUpdate() {
+	r.Mutex.RLock()
+	playerReprs := r.Gameplay.PlayerReprs()
+	message := OrderedKeysMarshal{
+		{"type", "assembly_update"},
+		{"players", playerReprs},
+	}
+	for _, conn := range r.Conns {
+		conn.OutChannel <- message
+	}
+	r.Mutex.RUnlock()
 }
 
 // Should be run in a goroutine
@@ -163,7 +202,7 @@ func GameRoomRun(room Room) {
 	GameRoomMap[room.Id] = r
 	GameRoomDataMutex.Unlock()
 
-	timeoutDur := 30 * time.Second
+	timeoutDur := 180 * time.Second
 	timeoutTicker := time.NewTicker(timeoutDur)
 	defer timeoutTicker.Stop()
 
@@ -172,14 +211,53 @@ func GameRoomRun(room Room) {
 
 loop:
 	for {
+	selection:
 		select {
 		case msg := <-r.InChannel:
-			r.Mutex.RLock()
-			conn := r.Conns[msg.UserId]
-			r.Mutex.RUnlock()
-			conn.OutChannel <- OrderedKeysMarshal{
-				{"message", "received"},
-				{"object", msg.Message},
+			message := msg.Message
+			if message["type"] == "seat" {
+				r.Mutex.Lock()
+				conn := r.Conns[msg.UserId]
+				user := conn.User
+				profileId, ok := message["profile_id"].(float64)
+				if !ok {
+					r.Mutex.Unlock()
+					conn.OutChannel <- OrderedKeysMarshal{{"error", "Incorrect `profile_id`"}}
+					break selection
+				}
+				profile := Profile{Id: int(profileId)}
+				if !profile.Load() {
+					r.Mutex.Unlock()
+					conn.OutChannel <- OrderedKeysMarshal{{"error", "No such profile"}}
+					break selection
+				}
+				if profile.Creator != user.Id {
+					r.Mutex.Unlock()
+					conn.OutChannel <- OrderedKeysMarshal{{"error", "Not creator"}}
+					break selection
+				}
+				if err := r.Gameplay.Seat(user, profile); err != "" {
+					r.Mutex.Unlock()
+					conn.OutChannel <- OrderedKeysMarshal{{"error", err}}
+					break selection
+				}
+				r.Mutex.Unlock()
+				r.BroadcastAssemblyUpdate()
+			} else if message["type"] == "withdraw" {
+				r.Mutex.Lock()
+				conn := r.Conns[msg.UserId]
+				if err := r.Gameplay.WithdrawSeat(msg.UserId); err != "" {
+					r.Mutex.Unlock()
+					conn.OutChannel <- OrderedKeysMarshal{{"error", err}}
+					break selection
+				}
+				r.Mutex.Unlock()
+				r.BroadcastAssemblyUpdate()
+			} else {
+				r.Mutex.RLock()
+				conn := r.Conns[msg.UserId]
+				r.Mutex.RUnlock()
+				conn.OutChannel <- OrderedKeysMarshal{{"error", "Unknown type"}}
 			}
 
 		case sig := <-r.Signal:
