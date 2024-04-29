8d76e89809deea2f9a99ddd124eab18a5c7b2a1f
Ayu <claire@ayu.land>
2024-04-30 01:37:50 +0800

    client: 🚧 Profiles

--- client/main.js
+++ client/main.js
@@ -3,7 +3,7 @@
 const api = 'http://localhost:10405'
 const apiUrl = new URL(api)
 const form = (dict) => {
-  const d = new FormData()
+  const d = new URLSearchParams()
   for (const [k, v] of Object.entries(dict))
     d.set(k, v)
   return d
@@ -29,6 +29,14 @@ const elStateMsg = document.getElementById('state-msg')
 const error = (msg) => { elStateMsg.innerText = msg; elStateMsg.classList = 'error' }
 const info = (msg) => { elStateMsg.innerText = msg; elStateMsg.classList = 'info' }
 
+const tryParse = (text) => {
+  try {
+    return JSON.parse(text)
+  } catch (e) {
+    return {}
+  }
+}
+
 let ws
 const reconnect = () => {
   ws = new WebSocket(
@@ -46,10 +54,23 @@ const reconnect = () => {
     console.log(o)
     if (o.type === 'log') {
       processLogs(o.log)
+    } else if (o.type === 'room_state') {
+      processLogs([{
+        id: -1,
+        timestamp: Math.floor(Date.now() / 1000),
+        content: `房间【${o.room.title}】：${o.room.description}`
+      }])
+      if (o.phase === 'assembly') {
+        showAssemblyPanel()
+      } else if (o.phase === 'appointment') {
+      } else if (o.phase === 'gameplay') {
+      }
     }
   }
 }
 reconnect()
+
+const send = (o) => ws.send(JSON.stringify(o))
 
 const htmlEscape = (s) =>
   s.replaceAll('&', '&amp;')
@@ -69,5 +90,50 @@ const processLogs = (logs) => {
     elContainer.appendChild(node)
   }
 }
+
+const showAssemblyPanel = () => {
+  document.getElementById('assembly-panel').classList.remove('hidden')
+  document.getElementById('gameplay-panel').classList.add('hidden')
+}
+const addProfileButton = (pf) => {
+  const elContainer = document.getElementById('profiles')
+  const node = document.createElement('button')
+  node.innerText = `Seat with profile [${pf.id}]: race ${pf.details.race}, desc ${pf.details.description}, stats ${pf.stats.join(', ')}`
+  node.addEventListener('click', (e) => {
+    send({
+      type: 'seat',
+      profile_id: pf.id,
+    })
+  })
+  elContainer.appendChild(node)
+}
+const profiles = await (await fetch(`${api}/profile/my`, { credentials: 'include' })).json()
+for (const pf of profiles) addProfileButton(pf)
+
+for (let i = 1; i <= 8; i++) {
+  const el = document.getElementById(`stat-${i}`)
+  const elValue = document.getElementById(`value-stat-${i}`)
+  el.addEventListener('input', (e) => {
+    elValue.innerText = el.value
+  })
+  elValue.innerText = el.value
+}
+document.getElementById('btn-new-profile').addEventListener('click', async (e) => {
+  const resp = await (await fetch(`${api}/profile/create`, {
+    credentials: 'include',
+    method: 'POST',
+    body: form({
+      details: JSON.stringify({
+        race: document.getElementById('pf-race').value,
+        description: document.getElementById('pf-background').value,
+      }),
+      stats: [1, 2, 3, 4, 5, 6, 7, 8]
+        .map((i) => document.getElementById(`stat-${i}`).value.toString())
+        .join(','),
+      traits: '',
+    }),
+  })).json()
+  addProfileButton(resp)
+})
 
 })()
